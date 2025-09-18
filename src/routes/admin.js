e45aa34214fd9069579ce37c50cb188536277dee
Jeffrey Herly <jeffreyherly9@gmail.com>
2025-09-18 15:44:05 +0700

    add route

---
+++ src/routes/admin.js
@@ -0,0 +1,8 @@
+export default async function adminRoutes(app, opts) {
+  app.get('/admin', async (req, reply) => {
+    console.log('masuk ke admin');
+    return reply.view('admin.njk', { title: 'Admin Page' });
+  });
+}
+
+
