90a3f6b812bc6a0aac3254b8e937550bc7c83e89
Jeffrey Herly <jeffreyherly9@gmail.com>
2025-09-23 13:29:28 +0700

    this is to make it sure if my program's fine

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ app.setErrorHandler(function (error, request, reply) {
 
 await registerRoutes(app);
 
-
 // root
 app.get('/', async (request, reply) => {
   // Redirect to /index
@@ -19,7 +18,7 @@ app.get('/', async (request, reply) => {
 // define server connection
 try {
   // start server at port 2000
-  app.listen({ port: 3000 });
+  app.listen({ port: 2000 });
 
   // give feedback that server is running
   app.log.info(`Server are now running at http://localhost:2000/`);
