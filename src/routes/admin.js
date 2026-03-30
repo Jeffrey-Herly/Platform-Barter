5d412b90bf901ad6cf7e66d35745849039f44681
Jeffrey <jeffreyherly9@gmail.com>
2026-03-30 21:55:46 +0700

    chore: clean up unused files and legacy src folder

--- src/routes/admin.js
+++
@@ -1,6 +0,0 @@
-export default async function adminRoutes(app, opts) {
-  app.get('/admin', async (req, reply) => {
-    console.log('masuk ke admin');
-    return reply.view('admin.njk', { title: 'Admin Page' });
-  });
-}
--- src/routes/routes.js
+++
@@ -1,9 +0,0 @@
-import indexRoutes from "./index.js";
-import adminRoutes from "./admin.js";
-import loginRoutes from "./login.js";
-
-export default async function registerRoutes(app) {
-  app.register(indexRoutes, { prefix: "/" });
-  app.register(adminRoutes, { prefix: "/" });
-  app.register(loginRoutes, { prefix: "/" });
-}
